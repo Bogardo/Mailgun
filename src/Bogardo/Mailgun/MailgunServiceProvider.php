f014c0a2d9e6c7b4ac7d5243f6168ffc3cf23185
Jarbit Lira <jarbitlira@gmail.com>
2015-09-15 16:12:49 -0600

    Correct path to access to config file

--- src/Bogardo/Mailgun/MailgunServiceProvider.php
+++ src/Bogardo/Mailgun/MailgunServiceProvider.php
@@ -20,7 +20,7 @@ class MailgunServiceProvider extends ServiceProvider
 	public function boot()
 	{
         $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('mailgun.php'),
+            __DIR__.'/../../config/config.php' => config_path('mailgun.php'),
         ]);
 	}
 
@@ -41,7 +41,7 @@ class MailgunServiceProvider extends ServiceProvider
 		});
 
         $this->mergeConfigFrom(
-            __DIR__.'/../config/config.php', 'mailgun'
+            __DIR__.'/../../config/config.php', 'mailgun'
         );
 	}
 
